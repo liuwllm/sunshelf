02e7ad8a42208d550826b1da4c2d0fb29078d9c6
liuwllm <liu.wllm@gmail.com>
2023-07-02 10:33:58 -0400

    Add Japanese word detection functionality

--- back/analysis/jpextract.py
+++ back/analysis/jpextract.py
@@ -1,19 +1,34 @@
+import textract
+import MeCab
+import regex as re
+
+def textExtract(path):
+    text = textract.process(path)
+    return text
+
 def jpWordExtract(text):
-    words = nagisa.wakati(text)
+    wakati = MeCab.Tagger("-Owakati")
+    wordList = wakati.parse(text).split()
     wordDict = {}
 
     for word in filter(
         checkWord,
-        words):
+        wordList):
         if word not in wordDict:
             wordDict[word] = 0
         else:
             wordDict[word] += 1
-
-        
+    
+    return wordDict
+      
 def checkWord(word):
     return (
         not re.match(r'^s*$', word)
         and not re.match(r'\W', word)
         and re.match(r'\p{Hiragana}|\p{Katakana}|\p{Han}', word)
     )
+
+extractedBytes = textExtract('./sample.txt')
+finalText = extractedBytes.decode("utf-8")
+
+print(jpWordExtract(finalText))
--- back/analysis/textextraction.py
+++
@@ -1,7 +0,0 @@
-import textract
-
-def textExtract(path):
-    text = textract.process(path)
-    return text
-    
-
