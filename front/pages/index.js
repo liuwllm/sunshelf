cdae3e0cf760d42e717bb36ff3d93ea92f761fef
liuwllm <liu.wllm@gmail.com>
2024-01-04 03:17:37 -0500

    Initialize Firebase login widget

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import FileUploadPage from '@/components/filesubmission'
 import Library from '@/components/library'
+import Login from '@/components/login'
 
 export default function Home() {
   return (
     <main className="bg-red-100 min-h-screen font-sans">
       <div>
+        <Login />
         <FileUploadPage />
         <Library />
       </div>
